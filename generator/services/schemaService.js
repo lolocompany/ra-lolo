9b4be4bb5044a837fb302d5be53c1a4d652cfba5
ex7r3me <siavash.ahmadpour@gmail.com>
2024-11-14 14:27:12 +0100

    Add support for array and object fields

--- generator/services/schemaService.js
+++ generator/services/schemaService.js
@@ -5,9 +5,12 @@ export const getSchema = async (resource) => {
   return await apiClient(url);
 };
 
-export const getProperties = (schema) => {
-  return Object.keys(schema.properties).map((key) => {
+export const getProperties = (schema, parentKey = "", forListView = false) => {
+  const fields = [];
+
+  Object.keys(schema.properties).forEach((key) => {
     const property = schema.properties[key];
+    const fieldName = parentKey ? `${parentKey}.${key}` : key; // Nested field name
     const name = property.title || key;
     let component;
     let choices;
@@ -42,28 +45,53 @@ export const getProperties = (schema) => {
         break;
 
       case "array":
-        if (property.items && property.items.type === "string") {
-          component = "ArrayInput";
-          choices = "<SimpleFormIterator><TextInput /></SimpleFormIterator>";
-        } else {
-          component = "ArrayInput";
-          choices = "<SimpleFormIterator>{ /* other inputs */ }</SimpleFormIterator>";
+        if (property.items) {
+          if (property.items.type === "string" && property.items.enum) {
+            component = "CheckboxGroupInput";
+            choices = property.items.enum.map((value) => ({ id: value, name: value }));
+          } else if (property.items.type === "string") {
+            component = "ArrayInput";
+            choices = "<SimpleFormIterator><TextInput /></SimpleFormIterator>";
+          } else if (property.items.type === "object") {
+            // Array of objects: Use SimpleFormIterator with nested fields
+            const nestedFields = getProperties(property.items, fieldName);
+            fields.push({
+              name,
+              value: fieldName,
+              component: "ArrayObjectSimpleFormIterator", // Custom marker for array of objects
+              properties: nestedFields,
+            });
+            return;
+          } else {
+            component = "ArrayInput";
+            choices = "<SimpleFormIterator>{ /* other inputs */ }</SimpleFormIterator>";
+          }
         }
         break;
 
-      // case "object":
-      //   component = "ObjectInput";
-      //   break;
+      case "object":
+        if (forListView) return; // Skip object fields for List view
+
+        // For Create view, add a section header with nested fields
+        fields.push({
+          name,
+          value: fieldName,
+          component: "NestedObjectSection",
+          properties: getProperties(property, fieldName),
+        });
+        return;
 
       default:
         component = "TextInput";
     }
 
-    return {
+    fields.push({
       name,
-      value: key,
+      value: fieldName,
       component,
       ...(choices && { choices }),
-    };
-  }).filter(Boolean);
+    });
+  });
+
+  return fields;
 };
