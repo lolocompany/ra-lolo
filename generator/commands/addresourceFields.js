ed2ec94d861146e8ed1dae1b0f3eafe5fd640f68
Sia <sia@lolo.company>
2024-11-15 16:15:30 +0100

    Add handlebar for file generation

--- generator/commands/addresourceFields.js
+++ generator/commands/addresourceFields.js
@@ -1,10 +1,11 @@
-import { program } from 'commander';
-import { promptResourceDetails } from '../utils/prompts.js';
-import { generateAllFields } from '../services/genereateFieldsService.js'
-import { getProperties, getSchema } from '../services/schemaService.js';
-import { applyTransformation } from '../services/transformService.js';
-import { APP_FILE_PATH } from '../utils/filePaths.js';
-import fs from 'fs';
+import { program } from "commander";
+import { promptResourceDetails } from "../utils/prompts.js";
+import { generateAllFields } from "../services/genereateFieldsService.js";
+import { getProperties, getSchema } from "../services/schemaService.js";
+// import { applyTransformation } from "../services/codeModService.js";
+import { generateResourceList } from "../services/fileGeneratorService.js";
+import { APP_FILE_PATH } from "../utils/filePaths.js";
+import fs from "fs";
 
 export const addResourceFieldsCommand = () => {
   program
@@ -14,14 +15,20 @@ export const addResourceFieldsCommand = () => {
       try {
         const resource = await promptResourceDetails();
         const schema = await getSchema(resource);
-        
+
         const stringProperties = getProperties(schema);
-        const selectedComponents = await generateAllFields(stringProperties);
-        
-        const sourceCode = fs.readFileSync(APP_FILE_PATH, "utf-8");
-        const transformedCode = applyTransformation(sourceCode, selectedComponents, resource);
-        
-        fs.writeFileSync(APP_FILE_PATH, transformedCode, "utf-8");
+        const selectedComponents = generateAllFields(stringProperties);
+        const components = ['TextField', 'EditButton', 'NumberField'];
+
+        generateResourceList(
+          components,
+          selectedComponents.listView
+        );
+
+        // const sourceCode = fs.readFileSync(APP_FILE_PATH, "utf-8");
+        // const transformedCode = applyTransformation(sourceCode, selectedComponents, resource);
+
+        // fs.writeFileSync(APP_FILE_PATH, transformedCode, "utf-8");
         console.log(`Selected fields added to the '${resource}' resource.`);
       } catch (error) {
         console.error("An error occurred:", error);
