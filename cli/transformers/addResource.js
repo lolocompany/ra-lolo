0b3c3d1d8f9e1118d9c721d78b8cb1adffaff107
Sia <sia@lolo.company>
2024-11-18 13:40:55 +0100

    extract resource template as a handlebar file

--- cli/transformers/addResource.js
+++ cli/transformers/addResource.js
@@ -1,28 +1,12 @@
 import jscodeshift from "jscodeshift";
 import fs from "fs";
+import Handlebars from "handlebars";
+import path from "path";
 
 export function addResource(filePath, resourceName, importPath) {
   const source = fs.readFileSync(filePath, "utf-8");
   const j = jscodeshift;
   const root = j(source);
-
-  const importExists = root.find(j.ImportDeclaration, {
-    source: { value: importPath },
-  }).size();
-
-  if (!importExists) {
-    const importDeclaration = j.importDeclaration(
-      [
-        j.importSpecifier(j.identifier("ProfileList")),
-        j.importSpecifier(j.identifier("ProfileCreate")),
-        j.importSpecifier(j.identifier("ProfileShow")),
-        j.importSpecifier(j.identifier("ProfileEdit")),
-      ],
-      j.literal(importPath)
-    );
-
-    root.find(j.ImportDeclaration).at(0).insertBefore(importDeclaration);
-  }
 
   const resourceExists = root
     .find(j.JSXElement, {
@@ -39,37 +23,18 @@ export function addResource(filePath, resourceName, importPath) {
     });
 
     if (adminNode.size() > 0) {
-      const resourceElement = j.jsxElement(
-        j.jsxOpeningElement(
-          j.jsxIdentifier("Resource"),
-          [
-            j.jsxAttribute(j.jsxIdentifier("name"), j.literal(resourceName)),
-            j.jsxAttribute(
-              j.jsxIdentifier("list"),
-              j.jsxExpressionContainer(j.identifier("ProfileList"))
-            ),
-            j.jsxAttribute(
-              j.jsxIdentifier("create"),
-              j.jsxExpressionContainer(j.identifier("ProfileCreate"))
-            ),
-            j.jsxAttribute(
-              j.jsxIdentifier("edit"),
-              j.jsxExpressionContainer(j.identifier("ProfileEdit"))
-            ),
-            j.jsxAttribute(
-              j.jsxIdentifier("show"),
-              j.jsxExpressionContainer(j.identifier("ProfileShow"))
-            ),
-          ],
-          true
-        ),
-        null,
-        []
-      );
+      
+      const templatePath = path.resolve("./templates/resourceComponent.jsx.hbs");
+      const templateContent = fs.readFileSync(templatePath, "utf-8");
+      const resourceTemplate = Handlebars.compile(templateContent);
+
+      const resourceCode = resourceTemplate({ resourceName });
+
+      const resourceElement = j(resourceCode).nodes();
 
       adminNode.forEach((path) => {
         const children = path.value.children || [];
-        children.push(resourceElement);
+        children.push(...resourceElement);
         path.value.children = children;
       });
     }
